95929fadf6ab1ecef1d19e0b7b30185dd557edf5
SnapolesIvan <144388463+SnapolesIvan@users.noreply.github.com>
2025-05-05 21:03:45 -0600

    Create db.js

---
+++ db.js
@@ -0,0 +1,8 @@
+// db.js
+import pkg from 'pg';
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  connectionString: 'postgresql://postgres:JHEfVlEQFDqqNvJbQfVCLxJsBCeIzXLI@postgres.railway.internal:5432/railway',
+  ssl: { rejectUnauthorized: false }
+});

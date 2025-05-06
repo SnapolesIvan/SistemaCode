79526a6009f36f86596744418d4c6b83eda38635
SnapolesIvan <144388463+SnapolesIvan@users.noreply.github.com>
2025-05-05 20:33:37 -0600

    Delete routes/contactoRoutes.js

--- routes/contactoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactoController = require('../controllers/contactoController');
-
-// Ruta para enviar mensaje de contacto
-router.post('/', contactoController.enviarContacto);
-
-module.exports = router;

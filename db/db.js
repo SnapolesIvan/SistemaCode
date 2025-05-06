0a6d975b6c7aa5353c5a3dcee34a7fcc90fdb75e
SnapolesIvan <144388463+SnapolesIvan@users.noreply.github.com>
2025-05-05 21:03:29 -0600

    Delete db directory

--- db/db.js
+++
@@ -1,7 +0,0 @@
-import pkg from 'pg';
-const { Pool } = pkg;
-
-export const pool = new Pool({
-  connectionString: 'postgresql://postgres:JHEfVlEQFDqqNvJbQfVCLxJsBCeIzXLI@postgres.railway.internal:5432/railway',
-  ssl: { rejectUnauthorized: false }
-});

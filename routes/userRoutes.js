77c018b9324c33d55fb6d5bf23275a36cc72e2ec
SnapolesIvan <144388463+SnapolesIvan@users.noreply.github.com>
2025-05-05 20:34:06 -0600

    Delete routes/userRoutes.js

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// Ruta para obtener perfil por ID
-router.get('/:id', userController.obtenerPerfil);
-
-module.exports = router;

17893961aca04b87d4f4deefd52a4265ec1e2718
SnapolesIvan <144388463+SnapolesIvan@users.noreply.github.com>
2025-05-06 11:58:17 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
+// ==== db.js ====
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:JHEfVlEQFDqqNvJbQfVCLxJsBCeIzXLI@postgres.railway.internal:5432/railway'
 });
 
 module.exports = pool;
